1943782a4849e1d0b8dff87e1e2a80487ba281bc
metherton <martinetherton63@gmail.com>
2016-01-05 08:24:29 +0100

    first commit

---
+++ app/scripts/controllers/about.js
@@ -0,0 +1,17 @@
+'use strict';
+
+/**
+ * @ngdoc function
+ * @name familysearchClientApp.controller:AboutCtrl
+ * @description
+ * # AboutCtrl
+ * Controller of the familysearchClientApp
+ */
+angular.module('familysearchClientApp')
+  .controller('AboutCtrl', function () {
+    this.awesomeThings = [
+      'HTML5 Boilerplate',
+      'AngularJS',
+      'Karma'
+    ];
+  });
---
+++ app/scripts/controllers/main.js
@@ -0,0 +1,17 @@
+'use strict';
+
+/**
+ * @ngdoc function
+ * @name familysearchClientApp.controller:MainCtrl
+ * @description
+ * # MainCtrl
+ * Controller of the familysearchClientApp
+ */
+angular.module('familysearchClientApp')
+  .controller('MainCtrl', function () {
+    this.awesomeThings = [
+      'HTML5 Boilerplate',
+      'AngularJS',
+      'Karma'
+    ];
+  });
